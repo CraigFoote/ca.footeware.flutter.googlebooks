f0b5af9ccc337b2547cce792ab4223acd28c24d9
CraigFoote <CraigFoote@gmail.com>
2021-12-22 18:00:35 -0500

    Initial commit

---
+++ android/app/src/main/java/ca/footeware/google_books/MainActivity.java
@@ -0,0 +1,6 @@
+package ca.footeware.google_books;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:google_books/custom_theme.dart';
+
+void main() {
+  runApp(const GoogleBooksApp());
+}
+
+class GoogleBooksApp extends StatefulWidget {
+  const GoogleBooksApp({Key? key}) : super(key: key);
+
+  @override
+  GoogleBooksAppState createState() => GoogleBooksAppState();
+}
+
+class GoogleBooksAppState extends State<GoogleBooksApp> {
+  final String title = 'Google Books';
+  ThemeData currentTheme = CustomTheme.lightTheme;
+
+  void themeCallback(value) {
+    setState(() => CustomTheme.currentTheme = value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      theme: CustomTheme.currentTheme,
+      debugShowCheckedModeBanner: false,
+      home: SearchPage(title, themeCallback),
+    );
+  }
+}
+
+class SearchPage extends StatefulWidget {
+  const SearchPage(this.title, this.themeCallback, {Key? key})
+      : super(key: key);
+  final String title;
+  final Function(ThemeData) themeCallback;
+
+  @override
+  State<StatefulWidget> createState() => SearchPageState();
+}
+
+class SearchPageState extends State<SearchPage> {
+  bool switchValue = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              width: 500,
+              child: TextField(
+                autofocus: true,
+                maxLength: 50,
+                enableSuggestions: true,
+              ),
+            ),
+            ElevatedButton.icon(
+              icon: Icon(Icons.search),
+              label: Text('Search'),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListView(
+            children: [
+              Row(
+                children: [
+                  const Text('Dark Theme'),
+                  Switch(
+                    value: switchValue,
+                    onChanged: (value) {
+                      setState(() {
+                        switchValue = value;
+                        widget.themeCallback(value
+                            ? CustomTheme.darkTheme
+                            : CustomTheme.lightTheme);
+                      });
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
