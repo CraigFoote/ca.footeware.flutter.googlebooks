1b0fbea81c6b7d8433426887c7d60cae2e8d26ea
CraigFoote <CraigFoote@gmail.com>
2021-12-22 22:39:44 -0500

    search and search results pages + dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:google_books/custom_theme.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const GoogleBooksApp());
@@ -43,6 +46,7 @@ class SearchPage extends StatefulWidget {
 
 class SearchPageState extends State<SearchPage> {
   bool switchValue = false;
+  final searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -56,9 +60,10 @@ class SearchPageState extends State<SearchPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SizedBox(
+            SizedBox(
               width: 500,
               child: TextField(
+                controller: searchController,
                 autofocus: true,
                 maxLength: 50,
                 enableSuggestions: true,
@@ -67,8 +72,15 @@ class SearchPageState extends State<SearchPage> {
             ElevatedButton.icon(
               icon: const Icon(Icons.search),
               label: const Text('Search'),
-              onPressed: () {},
-            )
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return SearchResultsPage(searchController.text);
+                  }),
+                );
+              },
+            ),
           ],
         ),
       ),
@@ -95,6 +107,135 @@ class SearchPageState extends State<SearchPage> {
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class SearchResultsPage extends StatelessWidget {
+  const SearchResultsPage(this.searchString, {Key? key}) : super(key: key);
+  final String searchString;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Google Books - Search Results for '" + searchString + "'"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: FutureBuilder<ListView>(
+          future: getSearchResults(searchString),
+          builder: (BuildContext context, AsyncSnapshot<ListView> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.data == null) {
+              return const Text('No search results found.');
+            } else {
+              var result = snapshot.data!;
+              return result;
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<ListView> getSearchResults(String searchString) async {
+    Uri uri = Uri(
+        scheme: 'https',
+        host: 'www.googleapis.com',
+        path: 'books/v1/volumes',
+        query: 'q=' + searchString);
+    Map<String, dynamic> result = json.decode(await http.read(uri));
+    var items = result['items'];
+    List<Card> cards = [];
+    for (var i = 0; i < items.length; i++) {
+      cards.add(createCard(items[i]));
+    }
+    return ListView.builder(
+        itemCount: cards.length,
+        itemBuilder: (context, index) {
+          return cards[index];
+        });
+  }
+
+  Card createCard(item) {
+    Map<String, dynamic> volumeInfo = item['volumeInfo'];
+    String title;
+    try {
+      title = volumeInfo['title'];
+    } catch (e) {
+      title = 'Unknown Title';
+    }
+    String authors;
+    try {
+      authors = volumeInfo['authors'].join(', ');
+    } catch (e) {
+      authors = 'Unknown Author(s)';
+    }
+    String publisher;
+    try {
+      publisher = volumeInfo['publisher'];
+    } catch (e) {
+      publisher = 'Unknown Publisher';
+    }
+    String publishedDate;
+    try {
+      publishedDate = volumeInfo['publishedDate'];
+    } catch (e) {
+      publishedDate = 'Unknown Publish Date';
+    }
+    var pageCount;
+    try {
+      pageCount = volumeInfo['pageCount'];
+    } catch (e) {
+      pageCount = 'Unknown Page Count';
+    }
+    return Card(
+      elevation: 5,
+      margin: const EdgeInsets.all(10.0),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  authors,
+                  style: const TextStyle(
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    publisher,
+                  ),
+                  Text(
+                    publishedDate,
+                  ),
+                  Text(
+                    pageCount.toString() + ' pages',
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
